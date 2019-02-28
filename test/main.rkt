5a10dcc512e5f2dfa9bef41fc217295afbe8fbe8
Stephen <stephen@thoughtstem.com>
2019-02-28 12:12:56 -0800

    removing ts-racket dependency

--- test/main.rkt
+++ test/main.rkt
@@ -1,28 +1,33 @@
 #lang racket
 
-(require "../languages/toy-lang.rkt"
-         #;"./languages/sound-lang.rkt")
+(module+ test 
 
-;The three things you'd want to do with
-;a visual language (after defining it.)
+  (require "../languages/toy-lang.rkt"
+           #;"./languages/sound-lang.rkt"
+           )
 
-;FOR STUDENTS:
-;Write with it
-(require ratchet/launch)
-(launch toy-lang-1)
+  ;The three things you'd want to do with
+  ;a visual language (after defining it.)
 
-
-;FOR TEACHERS:
-;Print its identifiers
-(tangeable-tiles toy-lang-1)
+  ;FOR STUDENTS:
+  ;Write with it
+  (require ratchet/launch)
+  (launch toy-lang-1)
 
 
-;FOR CURRICULUM DEVELOPERS
-;Write in the non visual form,
-;  but display in the visual form
-(typeset-code toy-lang-1
-              (rotate
-               (above (green circle)
-                      (red circle)
-                      (blue circle))))
+  ;FOR TEACHERS:
+  ;Print its identifiers
+  (tangeable-tiles toy-lang-1)
+
+
+  ;FOR CURRICULUM DEVELOPERS
+  ;Write in the non visual form,
+  ;  but display in the visual form
+  (typeset-code toy-lang-1
+                (rotate
+                  (above (green circle)
+                         (red circle)
+                         (blue circle))))
+
+  )
 
