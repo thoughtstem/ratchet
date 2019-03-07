1f023afd0b0adad7cb5a601669fd26d87c0ef9e9
Stephen <stephen@thoughtstem.com>
2019-03-07 13:19:29 -0800

    missing dep

--- info.rkt
+++ info.rkt
@@ -3,7 +3,7 @@
 (define scribblings 
   '(("scribblings/manual.scrbl" ())))
 
-(define deps '())
+(define deps '("read-lang-file"))
 
 (define drracket-tool-names (list "Tool Name"))
 (define drracket-tools (list (list "tools/main.rkt")))
