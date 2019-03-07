24f260af79104a765bde83a6b6db8f193b16b7cb
Stephen <stephen@thoughtstem.com>
2019-03-07 13:41:37 -0800

    fixing dep

--- info.rkt
+++ info.rkt
@@ -3,7 +3,7 @@
 (define scribblings 
   '(("scribblings/manual.scrbl" ())))
 
-(define deps '("read-lang-file"))
+(define deps '("lang-file"))
 
 (define drracket-tool-names (list "Tool Name"))
 (define drracket-tools (list (list "tools/main.rkt")))
